e0887435aa7b3b2701037db36b585d9c5805d0e7
Dalton125 <daltong10@gmail.com>
2014-11-18 15:02:59 -0600

    lab 15 100 point

--- 100pt.py
+++ 100pt.py
@@ -33,9 +33,24 @@ class MyApp:
 		self.up = Button(self.myContainer1)
 		self.up.configure(text="Up", background= "green")
 		self.up.grid(row=0,column=0)
+		
+		self.left = Button(self.myContainer1)
+		self.left.configure(text="left", background= "blue")
+		self.left.grid(row=0,column=1)
+		
+		self.right = Button(self.myContainer1)
+		self.right.configure(text="right", background= "yellow")
+		self.right.grid(row=0,column=2)
+		
+		self.down = Button(self.myContainer1)
+		self.down.configure(text="down", background= "red")
+		self.down.grid(row=0,column=3)
 					
 		# "Bind" an action to the first button												
 		self.up.bind("<Button-1>", self.moveUp)
+		self.left.bind("<Button-1>", self.moveleft)
+		self.right.bind("<Button-1>", self.moveright)
+		self.down.bind("<Button-1>", self.movedown)
                 
 		  
 		# This creates the drawpad - no need to change this 
@@ -46,21 +61,68 @@ class MyApp:
 	def moveUp(self, event):   
 		global player
 		global drawpad
-                drawpad.move(player,0,-10)
+		global drawpadwidth
+                global drawpadheight
+		x1,y1,x2,y2 = drawpad.coords(player)
+		if y1 <= 13:
+		    drawpad.move(player,0,0)
+		else:
+		    drawpad.move(player,0,-10)
+        def moveleft(self, event):   
+		global player
+		global drawpad
+		global drawpadwidth
+                global drawpadheight
+		x1,y1,x2,y2 = drawpad.coords(player)
+		if x1 <= 10:
+		    drawpad.move(player,0,0)
+		else:
+		    drawpad.move(player,-10,0)
+        def moveright(self, event):   
+		global player
+		global drawpad
+		global drawpadwidth
+		global drawpadheight
+		x1,y1,x2,y2 = drawpad.coords(player)
+		if x2 >= 480:
+		    drawpad.move(player,0,0)
+		else:
+		    drawpad.move(player,10,0)
+        def movedown(self, event):   
+		global player
+		global drawpad
+		global drawpadwidth
+                global drawpadheight
+		x1,y1,x2,y2 = drawpad.coords(player)
+		if y2 >= 320:
+		    drawpad.move(player,0,0)
+		else:
+		    drawpad.move(player,0,10)
     
          
-        # Animate function that will bounce target left and right, and trigger the collision detection  
+        # Animate function that will bounce target left and right, and trigger the collision detection
+        # Insert the code here to make the target move, bouncing on the edges    
 	def animate(self):
 	    global target
 	    global direction
+	    global drawpad
+	    x1, y1, x2, y2 = drawpad.coords(target)
+            if x2 > drawpad.winfo_width(): 
+                direction = -4
+            if x1 < 0:
+                direction = 4
+            drawpad.move(target,direction,0)
 	    
-	    # Insert the code here to make the target move, bouncing on the edges    
-	        
-	        
-            
-            
+	     
             #  This will trigger our collision detect function
             didWeHit = self.collisionDetect()
+            if didWeHit == True:
+                drawpad.move(target,0,0)
+                drawpad.itemconfig(target, fill ="blue")
+            else:
+                drawpad.itemconfig(target, fill ="red")
+                drawpad.after(10,self.animate)
+
             # Use the value of didWeHit to create an if statement
             # that determines whether to run drawpad.after(1,self.animate) or not
             
@@ -69,6 +131,12 @@ class MyApp:
                 global target
 		global drawpad
                 global player
+                global didWeHit
+                global target, tx1,ty1,tx2,ty2
+                tx1,ty1,tx2,ty2 = drawpad.coords(target)
+                x1, y1, x2, y2 = drawpad.coords(player)
+                if x1 >= tx1 and x2 <= tx2 and y1 >= ty1 and y2 <= ty2:
+                    return True 
                 # Get the co-ordinates of our player AND our target
                 # using x1,y1,x2,y2 = drawpad.coords(object)
 
